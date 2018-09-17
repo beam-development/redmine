12fb69fb63f9b468644396c42cc329c9e29875ef
Robert Gornall <robertgo@beam.tv>
2018-09-17 14:47:10 +0100

    Add redmine mail checker plugin

---
+++ plugins/redmine_mail_checker/README.md
@@ -0,0 +1,51 @@
+## Redmine Mail Checker Plugin
+
+[![GitHub license](https://img.shields.io/github/license/jbox-web/redmine_mail_checker.svg)](https://github.com/jbox-web/redmine_mail_checker/blob/devel/LICENSE)
+[![GitHub release](https://img.shields.io/github/release/jbox-web/redmine_mail_checker.svg)](https://github.com/jbox-web/redmine_mail_checker/releases/latest)
+[![Code Climate](https://codeclimate.com/github/jbox-web/redmine_mail_checker.png)](https://codeclimate.com/github/jbox-web/redmine_mail_checker)
+
+### A Redmine plugin to check your mails, easy ;)
+
+This plugin replaces Redmine rake task to check mails.
+
+## Why?
+
+Running periodicaly ```bundle exec rake redmine:email:receive_imap``` to check your mails leads to high memory consumption : Redmine is loaded twice, once for your webserver and once for the cron task.
+This is not very efficient : for VPS with small memory size this can lead to memory saturation and the intervention of the OOM Killer who will kill the job.
+
+## Compatibility
+
+This plugin is compatible with Redmine versions > **2.4.x**.
+
+## Installation
+
+**1)** Install this plugin with git clone
+
+```sh
+redmine$ cd REDMINE_ROOT/plugins
+redmine$ git clone https://github.com/jbox-web/redmine_mail_checker.git
+redmine$ cd redmine_mail_checker/
+redmine$ git checkout 1.0.0
+```
+
+*No need of db:migrate or bundle update, just restart Redmine.*
+
+**2)** Enable WS for mail reception (in *Administration -> Settings -> Incoming emails*) and generate an API key.
+
+**3)** Configure your mailbox settings (in *Administration -> Plugins -> Configure*)
+
+**4)** Then configure your crontab to call a specific url to trigger mailbox check instead of ```bundle exec rake redmine:email:receive_imap```
+
+```sh
+curl http://redmine.example.net/check_mails?key=<API_KEY>
+```
+
+That's all!
+
+## Contribute
+
+You can contribute to this plugin in many ways such as :
+* Helping with documentation
+* Contributing code (features or bugfixes)
+* Reporting a bug
+* Submitting translations
---
+++ plugins/redmine_mail_checker/app/controllers/mail_checker_controller.rb
@@ -0,0 +1,99 @@
+class MailCheckerController < ApplicationController
+  unloadable
+
+  skip_before_filter :verify_authenticity_token
+  skip_before_filter :check_if_login_required
+
+  before_filter :check_credential
+
+  accept_api_auth :index
+
+
+  def index
+    if protocol == 'imap'
+      method = :check_imap_mails
+    else
+      method = :check_pop_mails
+    end
+
+    RedmineMailChecker.logger.info("Checking #{protocol.upcase} mails")
+
+    begin
+      self.send(method)
+    rescue Exception => e
+      RedmineMailChecker.logger.error(e.message)
+    else
+      RedmineMailChecker.logger.info('Done!')
+    end
+
+    render nothing: true
+  end
+
+
+  private
+
+
+    def protocol
+      RedmineMailChecker.get_setting('protocol')
+    end
+
+
+    def check_credential
+      User.current = nil
+      unless Setting.mail_handler_api_enabled? && params[:key].to_s == Setting.mail_handler_api_key
+        render text: 'Access denied. Incoming emails WS is disabled or key is invalid.', status: 403
+      end
+    end
+
+
+    def check_imap_mails
+      Redmine::IMAP.check(common_options.merge(imap_options), MailHandler.extract_options_from_env(redmine_options))
+    end
+
+
+    def check_pop_mails
+      Redmine::POP3.check(common_options.merge(pop_options), MailHandler.extract_options_from_env(redmine_options))
+    end
+
+
+    def common_options
+      {
+        :host     => RedmineMailChecker.get_setting('host'),
+        :port     => RedmineMailChecker.get_setting('port').to_i,
+        :ssl      => RedmineMailChecker.get_setting('ssl', true),
+        :username => RedmineMailChecker.get_setting('username'),
+        :password => RedmineMailChecker.get_setting('password')
+      }
+    end
+
+
+    def imap_options
+      options = {}
+      options[:folder]          = !imap_folder.empty? ? imap_folder : nil
+      options[:move_on_success] = RedmineMailChecker.get_setting('move_on_success')
+      options[:move_on_failure] = RedmineMailChecker.get_setting('move_on_failure')
+      options
+    end
+
+
+    def imap_folder
+      RedmineMailChecker.get_setting('folder')
+    end
+
+
+    def pop_options
+      options = {}
+      options[:apop] = RedmineMailChecker.get_setting('apop', true),
+      options[:delete_unprocessed] = RedmineMailChecker.get_setting('delete_unprocessed', true)
+      options
+    end
+
+
+    def redmine_options
+      options = {}
+      options['project']        = RedmineMailChecker.get_setting('project')
+      options['allow_override'] = RedmineMailChecker.get_setting('allow_override')
+      options
+    end
+
+end
